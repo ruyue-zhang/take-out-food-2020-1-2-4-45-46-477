10040f2625ed338035555daed4cb8343117a1d5c
lpdft <772346730@qq.com>
2020-01-04 23:39:48 +0800

    [ruyue] feat-takeOutOfFood: get selected object and add count&singleTotal

--- src/best-charge.js
+++ src/best-charge.js
@@ -1,3 +1,20 @@
-function bestCharge(selectedItems) {
-  return /*TODO*/;
+function bestCharge(inputs) {
+  let allItems = loadAllItems();
+  let selectedObject = getDishshesByInput(inputs,allItems);
 }
+
+function getDishshesByInput(selectedItems,allItems) {
+  var selectedObjArr = [];
+  selectedItems.forEach(valueSelected=>{
+    allItems.forEach(valueItem=>{
+      if(valueSelected.substring(0,8) === valueItem.id) {
+        valueItem.count = Number(valueSelected.substring(11));
+        valueItem.singleTotal = Number(valueItem.count * valueItem.price);
+        selectedObjArr.push(valueItem);
+      }
+    });
+  });
+  return selectedObj;
+}
+
+
