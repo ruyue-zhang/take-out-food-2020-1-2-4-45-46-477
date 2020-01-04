f3e73786d88e5098d6665187bbb65e7df4c3998f
lpdft <772346730@qq.com>
2020-01-04 23:48:50 +0800

    [ruyue] feat-takeOutOfFood: add add is specific flag

--- src/best-charge.js
+++ src/best-charge.js
@@ -1,20 +1,27 @@
 function bestCharge(inputs) {
   let allItems = loadAllItems();
+  let allDiscount = loadPromotions();
   let selectedObject = getDishshesByInput(inputs,allItems);
+  addIsSpecificFlag(selectedObject,allDiscount);
+  return selectedObject;
 }
 
 function getDishshesByInput(selectedItems,allItems) {
-  var selectedObjArr = [];
+  var selectedObject = [];
   selectedItems.forEach(valueSelected=>{
     allItems.forEach(valueItem=>{
       if(valueSelected.substring(0,8) === valueItem.id) {
         valueItem.count = Number(valueSelected.substring(11));
         valueItem.singleTotal = Number(valueItem.count * valueItem.price);
-        selectedObjArr.push(valueItem);
+        selectedObject.push(valueItem);
       }
     });
   });
-  return selectedObj;
+  return selectedObject;
+}
+
+function addIsSpecificFlag(selectedObjArr,allDiscount) {
+  selectedObjArr.forEach(value=>value.isSpecific = (-1 !== allDiscount[1].items.indexOf(value.id)));
 }
 
 
